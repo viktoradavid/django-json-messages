48e0633202d2785b827f1b118ab360e89a1629ee
matiboy <mathieu.chauvinc@gmail.com>
2015-02-25 10:33:39 +0800

    Leftovers from tests removed

--- json_messages/context_processors.py
+++ json_messages/context_processors.py
@@ -3,8 +3,5 @@ import django.contrib.messages
 def json_messages(request):
   messages = django.contrib.messages.get_messages(request)
   return {
-    	'json_messages' : [{"msg": "<b>Happy HTML</b>", "type": 'danger'}]
-	}
-  return {
     'json_messages': map(lambda x: {'msg': str(x), 'type': x.tags}, messages)
   }
